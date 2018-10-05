07068161345105ab83daff123d9d9c1e344d43f0
maoqitian <532977849@qq.com>
2018-10-05 16:42:18 +0800

    add license

---
+++ app/src/main/java/mao/com/mao_wanandroid_client/application/MyApplication.java
@@ -0,0 +1,15 @@
+package mao.com.mao_wanandroid_client.application;
+
+import android.app.Application;
+
+/**
+ * @author maoqitian
+ * @Description
+ * @Time 2018/9/30 0030 16:39
+ */
+public class MyApplication extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
