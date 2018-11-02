caee1a6079cf4a2191ca3ecad8ec29cdedfb14c6
maoqitian <maoqitian068@163.com>
2018-11-02 21:23:05 +0800

    finish db module

--- app/src/main/java/mao/com/mao_wanandroid_client/core/db/DbHelper.java
+++ app/src/main/java/mao/com/mao_wanandroid_client/core/db/DbHelper.java
@@ -1,5 +1,9 @@
 package mao.com.mao_wanandroid_client.core.db;
 
+import java.util.List;
+
+import mao.com.mao_wanandroid_client.core.dao.SearchHistoryData;
+
 /**
  * @author maoqitian
  * @Description
@@ -7,4 +11,21 @@ package mao.com.mao_wanandroid_client.core.db;
  */
 public interface DbHelper {
 
+    /**
+     * 添加搜索历史数据
+     * @param data
+     * @return
+     */
+    List<SearchHistoryData> addSearchHistoryData(String data);
+
+    /**
+     * 加载所有历史数据
+     * @return
+     */
+    List<SearchHistoryData> loadAllSearchHistoryData();
+
+    /**
+     * 清理所有搜索历史
+     */
+    void clearAllSearchHistoryData();
 }
