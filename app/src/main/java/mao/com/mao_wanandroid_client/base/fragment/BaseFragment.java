0dd1841df107d2faac643ddc4b8dca057bfbb6dc
maohomePc <maoqitian068@163.com>
2018-12-18 23:10:30 +0800

    update BaseFragment

--- app/src/main/java/mao/com/mao_wanandroid_client/base/fragment/BaseFragment.java
+++ app/src/main/java/mao/com/mao_wanandroid_client/base/fragment/BaseFragment.java
@@ -1,5 +1,11 @@
 package mao.com.mao_wanandroid_client.base.fragment;
 
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.view.View;
+
 import javax.inject.Inject;
 
 import mao.com.mao_wanandroid_client.base.BaseView;
@@ -16,7 +22,35 @@ public class BaseFragment <T extends AbstractBasePresenter> extends SimpleFragme
     @Inject
     private T mPresenter;
 
+    @Override
+    public void onAttach(Activity activity) {
+        super.onAttach(activity);
 
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        if(mPresenter != null){
+            mPresenter.attachView(this);
+        }
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        if(mPresenter != null){
+            mPresenter.detachView();
+        }
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        if(mPresenter != null){
+            mPresenter = null;
+        }
+    }
 
     @Override
     public void showNormal() {
