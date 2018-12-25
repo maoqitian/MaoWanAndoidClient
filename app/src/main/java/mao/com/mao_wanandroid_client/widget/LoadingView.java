478402d3b0f4ef521675e49f3175b4eb5da70571
maoqitian <maoqitian068@163.com>
2018-12-25 23:19:26 +0800

    add LoadingView、ShapeView(unfinish)

---
+++ app/src/main/java/mao/com/mao_wanandroid_client/widget/LoadingView.java
@@ -0,0 +1,26 @@
+package mao.com.mao_wanandroid_client.widget;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.LinearLayout;
+
+/**
+ * @author maoqitian
+ * @Description 酷炫加载view (未实现  https://www.imooc.com/article/51132)
+ * @Time 2018/12/25 0025 20:14
+ */
+public class LoadingView extends LinearLayout {
+    public LoadingView(Context context) {
+        super(context);
+    }
+
+    public LoadingView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public LoadingView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+
+}
---
+++ app/src/main/java/mao/com/mao_wanandroid_client/widget/ShapeView.java
@@ -0,0 +1,25 @@
+package mao.com.mao_wanandroid_client.widget;
+
+import android.content.Context;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * @author maoqitian
+ * @Description 自定义加载动画中的 图形
+ * @Time 2018/12/25 0025 20:13
+ */
+public class ShapeView extends View {
+    public ShapeView(Context context) {
+        super(context);
+    }
+
+    public ShapeView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public ShapeView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+}
