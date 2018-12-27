e427c45b2bd7b5aea3b898dd2d3fd70b5b4b7efa
maoqitian <maoqitian068@163.com>
2018-12-27 23:31:28 +0800

    update base

--- app/src/main/java/mao/com/mao_wanandroid_client/base/activity/RootActivity.java
+++ app/src/main/java/mao/com/mao_wanandroid_client/base/activity/RootActivity.java
@@ -1,8 +1,12 @@
 package mao.com.mao_wanandroid_client.base.activity;
 
 import android.text.TextUtils;
+import android.view.View;
+import android.view.ViewGroup;
 
+import mao.com.mao_wanandroid_client.R;
 import mao.com.mao_wanandroid_client.base.presenter.RxBasePresenter;
+import mao.com.mao_wanandroid_client.widget.LoadingView;
 
 /**
  * @author maoqitian
@@ -15,9 +19,21 @@ public class RootActivity<T extends RxBasePresenter> extends BaseActivity<T> {
     //默认为NORMAL状态
     private int currentState = STATE_NORMAL;
 
+    private LoadingView mLoadingView;
+
+    private ViewGroup normalView;
+    private View loadingView;
+    private View errorView;
+
     @Override
     protected void initEventAndData() {
         super.initEventAndData();
+        normalView = findViewById(R.id.view_base_normal);
+        if (normalView == null) {
+            throw new IllegalStateException(
+                    "The subclass of RootActivity must contain a View named 'normalView'.");
+        }
+
     }
 
 
