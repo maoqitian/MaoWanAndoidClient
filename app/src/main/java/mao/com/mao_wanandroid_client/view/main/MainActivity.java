6cdec8d7e087c2a2b66b6da0ab78c4171179a398
maoqitian <maoqitian068@163.com>
2019-04-21 23:13:18 +0800

    add drawlayout

--- app/src/main/java/mao/com/mao_wanandroid_client/view/main/MainActivity.java
+++ app/src/main/java/mao/com/mao_wanandroid_client/view/main/MainActivity.java
@@ -1,21 +1,41 @@
 package mao.com.mao_wanandroid_client.view.main;
 
-import android.support.v7.app.AppCompatActivity;
+import android.support.annotation.NonNull;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.NavigationView;
+import android.support.design.widget.Snackbar;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.view.KeyEvent;
-import android.view.MotionEvent;
+import android.view.MenuItem;
+import android.view.View;
 import android.widget.Toast;
 
 import java.util.Timer;
 import java.util.TimerTask;
 
+import butterknife.BindView;
 import mao.com.mao_wanandroid_client.R;
 import mao.com.mao_wanandroid_client.base.activity.BaseActivity;
 import mao.com.mao_wanandroid_client.presenter.main.MainContract;
 import mao.com.mao_wanandroid_client.presenter.main.MainPresenter;
 
 
-public class MainActivity extends BaseActivity<MainPresenter> implements MainContract.MainView {
+public class MainActivity extends BaseActivity<MainPresenter>
+        implements MainContract.MainView ,
+        NavigationView.OnNavigationItemSelectedListener {
+
+    @BindView(R.id.toolbar)
+    Toolbar toolbar;
+    @BindView(R.id.fab)
+    FloatingActionButton fab;
+    @BindView(R.id.drawer_layout)
+    DrawerLayout drawer;
+    @BindView(R.id.nav_view)
+    NavigationView navigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,11 +56,31 @@ public class MainActivity extends BaseActivity<MainPresenter> implements MainCon
                         Log.e("mao",e.getMessage()+errorMsg);
                     }
                 });*/
+
+        setSupportActionBar(toolbar);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+        navigationView.setNavigationItemSelectedListener(this);
     }
 
     @Override
     protected int getLayout() {
         return R.layout.activity_main;
+    }
+
+
+    @Override
+    public void onBackPressedSupport() {
+        super.onBackPressedSupport();
     }
 
     //再点一次退出程序
@@ -71,5 +111,19 @@ public class MainActivity extends BaseActivity<MainPresenter> implements MainCon
             finish();
             System.exit(0);
         }
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        int id = menuItem.getItemId();
+
+        if (id == R.id.nav_collect) {
+            // Handle the camera action
+        } else if (id == R.id.nav_settings) {
+
+        }
+        //DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
     }
 }
