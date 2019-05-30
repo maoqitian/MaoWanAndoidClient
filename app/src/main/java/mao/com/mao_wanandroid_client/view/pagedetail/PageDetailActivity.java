26e82759c3f7fa7b0f1fc613104379d7fceec41e
maoqitian <maoqitian068@163.com>
2019-05-31 00:03:10 +0800

    add PageDetailActivity

---
+++ app/src/main/java/mao/com/mao_wanandroid_client/view/pagedetail/PageDetailActivity.java
@@ -0,0 +1,10 @@
+package mao.com.mao_wanandroid_client.view.pagedetail;
+
+/**
+ * @author maoqitian
+ * @Description 详情页
+ * @Time 2019/5/31 0031 0:01
+ */
+public class PageDetailActivity {
+    
+}
