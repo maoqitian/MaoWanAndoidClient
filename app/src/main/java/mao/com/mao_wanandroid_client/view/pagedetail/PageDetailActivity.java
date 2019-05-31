1c3067e8aa253ee4b93b1b76c7a52d7e792e78e3
maoqitian <maoqitian068@163.com>
2019-05-31 23:06:09 +0800

    add page detail layout

--- app/src/main/java/mao/com/mao_wanandroid_client/view/pagedetail/PageDetailActivity.java
+++ app/src/main/java/mao/com/mao_wanandroid_client/view/pagedetail/PageDetailActivity.java
@@ -5,6 +5,6 @@ package mao.com.mao_wanandroid_client.view.pagedetail;
  * @Description 详情页
  * @Time 2019/5/31 0031 0:01
  */
-public class PageDetailActivity {
-    
+public class PageDetailActivity  {
+
 }
