4ad8c01ec4abe3a1bde40ee86bb80ad0a0210d38
maoqitian <maoqitian068@163.com>
2019-07-14 23:51:03 +0800

    solve navigationfragment bug

--- app/src/main/java/mao/com/mao_wanandroid_client/view/main/fragment/NavigationFragment.java
+++ app/src/main/java/mao/com/mao_wanandroid_client/view/main/fragment/NavigationFragment.java
@@ -1,11 +1,13 @@
 package mao.com.mao_wanandroid_client.view.main.fragment;
 
+import android.annotation.SuppressLint;
 import android.content.ContextWrapper;
 import android.support.annotation.NonNull;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
+import android.view.MotionEvent;
 import android.view.View;
 
 import com.scwang.smartrefresh.header.MaterialHeader;
@@ -31,7 +33,9 @@ import q.rorbin.verticaltablayout.widget.TabView;
 
 /**
  * @author maoqitian
- * @Description 导航
+ * @Description 导航  VerticalTabLayout +  recyclerView 联动
+ * 1、监听recyclerView滑动到的位置，tablayout切换到对应标签
+ * 2、tablayout各标签点击，recyclerView可滑动到对应区域
  * @Time 2019/5/8 0008 23:33
  */
 public class NavigationFragment  extends BaseFragment<NavigationPresenter> implements NavigationContract.NavigationView {
@@ -52,10 +56,16 @@ public class NavigationFragment  extends BaseFragment<NavigationPresenter> imple
     //下拉刷新头部
     private MaterialHeader mMaterialHeader;
 
-    private int currentPosition = 0;
-    private VerticalTabLayout.OnTabSelectedListener tabSelectedListener;
 
-    private boolean isClick = false;
+    private VerticalTabLayout.OnTabSelectedListener tabSelectedListener;
+    private RecyclerView.OnScrollListener onScrollListener;
+    
+    //用于recyclerView滑动到指定的位置
+    private boolean canScroll;
+    //是否点击了 Tab
+    private boolean isClickTab;
+    //指向位置
+    private int indexPosition;
 
     @Override
     protected int getLayoutId() {
@@ -78,27 +88,40 @@ public class NavigationFragment  extends BaseFragment<NavigationPresenter> imple
         mAdapter = new NavigationViewAdapter(R.layout.nav_item_cardview_layout);
         mRecyclerView.setAdapter(mAdapter);
         SmartRefreshLayoutListener();
-        VerticalTabLayoutWithRecyclerView();
+
     }
 
-    private int recyclerHeight;
-
+    @SuppressLint("ClickableViewAccessibility")
     private void VerticalTabLayoutWithRecyclerView() {
+         onScrollListener =new RecyclerView.OnScrollListener() {
+            // RecyclerView 滚动状态变化时回调
+            @Override
+            public void onScrollStateChanged(@NonNull RecyclerView recyclerView, int newState) {
+                super.onScrollStateChanged(recyclerView, newState);
+                if (canScroll && (newState == RecyclerView.SCROLL_STATE_IDLE)) {
+                    canScroll = false;
+                    //moveToPosition(layoutManager, recyclerView, scrollToPosition);
+                    RecyclerViewSmoothScroll();
+                }
+                //RecyclerView 滑动关联 VerticalTabLayout
+                RecyclerViewWithTabLayout(newState);
+            }
+            // RecyclerView 滚动时回调
+            @Override
+            public void onScrolled(@NonNull RecyclerView recyclerView, int dx, int dy) {
+                super.onScrolled(recyclerView, dx, dy);
+                if(canScroll){
+                    canScroll = false;
+                    RecyclerViewSmoothScroll();
+                }
+            }
+        };
         tabSelectedListener = new VerticalTabLayout.OnTabSelectedListener() {
             @Override
             public void onTabSelected(TabView tab, int position) {
-                isClick = true;
-                LinearLayoutManager l = (LinearLayoutManager) mRecyclerView.getLayoutManager();
-                int firstPosition = l.findFirstVisibleItemPosition();
-                int lastPosition = l.findLastVisibleItemPosition();
-                if (position > lastPosition) {
-                    mRecyclerView.smoothScrollToPosition(position);
-                } else if (position < firstPosition) {
-                    mRecyclerView.smoothScrollToPosition(position);
-                } else {
-                    int top = mRecyclerView.getChildAt(position - firstPosition).getTop();
-                    mRecyclerView.smoothScrollBy(0, top);
-                }
+                isClickTab = false;
+                //点击标签，使recyclerView滑动
+                moveToPosition(layoutManager, mRecyclerView, position);
             }
 
             @Override
@@ -106,31 +129,66 @@ public class NavigationFragment  extends BaseFragment<NavigationPresenter> imple
 
             }
         };
-        RecyclerView.OnScrollListener onScrollListener =new RecyclerView.OnScrollListener() {
-            @Override
-            public void onScrollStateChanged(@NonNull RecyclerView recyclerView, int newState) {
-                super.onScrollStateChanged(recyclerView, newState);
-                if (newState == 0) {
-                    isClick = false;
-                }
-            }
-
-            @Override
-            public void onScrolled(@NonNull RecyclerView recyclerView, int dx, int dy) {
-                super.onScrolled(recyclerView, dx, dy);
-                if (!isClick) {
-                    LinearLayoutManager l = (LinearLayoutManager) recyclerView.getLayoutManager();
-                    int firstPosition = l.findFirstVisibleItemPosition();
-                    int lastPosition = l.findLastVisibleItemPosition();
-                    int allItems = l.getItemCount();
-                    verticalTabLayout.setVerticalScrollbarPosition(firstPosition);
-                    Log.i("zkd", "[TablayoutWithRecyclerActivity][onScrollStateChanged]==> firstPosition : " + firstPosition + ",\nlastPosition:" + lastPosition + ",\nall:" + allItems);
-                }
-            }
-        };
-        verticalTabLayout.addOnTabSelectedListener(tabSelectedListener);
         mRecyclerView.addOnScrollListener(onScrollListener);
+        verticalTabLayout.addOnTabSelectedListener(tabSelectedListener);
     }
+    //滑动到对应位置
+    private void RecyclerViewSmoothScroll() {
+        int indexPositionDistance = indexPosition - layoutManager.findFirstVisibleItemPosition();
+        if (indexPositionDistance >= 0 && indexPositionDistance < mRecyclerView.getChildCount()) {
+            int top = mRecyclerView.getChildAt(indexPositionDistance).getTop();
+            mRecyclerView.smoothScrollBy(0, top);
+        }
+    }
+
+    private void RecyclerViewWithTabLayout(int newState) {
+        if (newState == RecyclerView.SCROLL_STATE_IDLE) {
+            if (isClickTab) {
+                //点击tab 不执行以下操作
+                isClickTab = false;
+                return;
+            }
+            int firstPosition = layoutManager.findFirstVisibleItemPosition();
+            if (indexPosition != firstPosition) {
+                indexPosition = firstPosition;
+                //使 TabLayout 选择正确的item
+                setTabLayoutChecked(indexPosition);
+            }
+        }
+    }
+
+    private void setTabLayoutChecked(int position) {
+        if (isClickTab) {
+            isClickTab = false;
+        } else {
+            if (verticalTabLayout == null) {
+                return;
+            }
+            verticalTabLayout.setTabSelected(indexPosition);
+        }
+        indexPosition = position;
+    }
+    //VerticalTabLayout 点击使 RecyclerView 滑动到对应位置
+    private void moveToPosition(LinearLayoutManager layoutManager, RecyclerView mRecyclerView, int position) {
+        // 第一个可见的view的位置
+        int firstItem = layoutManager.findFirstVisibleItemPosition();
+        // 最后一个可见的view的位置
+        int lastItem = layoutManager.findLastVisibleItemPosition();
+        if (position <= firstItem) {
+            // 如果跳转位置firstItem 之前(滑出屏幕的情况)，就smoothScrollToPosition可以直接跳转，
+            mRecyclerView.smoothScrollToPosition(position);
+        } else if (position <= lastItem) {
+            // 跳转位置在firstItem 之后，lastItem 之间（显示在当前屏幕），smoothScrollBy来滑动到指定位置
+            int top = mRecyclerView.getChildAt(position - firstItem).getTop();
+            mRecyclerView.smoothScrollBy(0, top);
+        } else {
+            // 如果要跳转的位置在lastItem 之后，则先调用smoothScrollToPosition将要跳转的位置滚动到可见位置
+            // 再通过onScrollStateChanged控制再次调用当前moveToPosition方法，执行上一个判断中的方法
+            mRecyclerView.smoothScrollToPosition(position);
+            canScroll = true;
+        }
+    }
+
 
     private void SmartRefreshLayoutListener() {
         mMaterialHeader = (MaterialHeader)mSmartRefreshLayout.getRefreshHeader();
@@ -190,6 +248,7 @@ public class NavigationFragment  extends BaseFragment<NavigationPresenter> imple
             }
         });
         mAdapter.replaceData(mNavigationListData);
+        VerticalTabLayoutWithRecyclerView();
     }
 
 
